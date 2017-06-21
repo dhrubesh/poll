d6009a8eba48a4f7d0d0e0694a600344c9369dfc
dhrubesh <dhrubesh97@gmail.com>
2017-06-21 17:01:17 +0000

    mongodb set-up

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var mongoose = require('mongoose')
 var morgan = require('morgan')
 var bodyParser = require('body-parser')
 var dotenv = require('dotenv')
-
+mongoose.connect('mongodb://localhost/voting')
 dotenv.config({verbose:true})
 
 const app = express()
@@ -14,4 +14,22 @@ app.get('/', function (req, res) {
 
 app.listen(8080, function () {
   console.log(' listening on port 8080...')
+})
+
+mongoose.connect(db,function(err){
+  if(err){
+    console.log(err);
+  }
+});
+
+mongoose.connection.on('connected',function(){
+  console.log('successfully connected  a connection at '+db);
+})
+
+mongoose.connection.on('disconnected',function(){
+  console.log('successfully disconnected from '+db);
+})
+
+mongoose.connection.on('error',function(){
+  console.log('An error has occured connecting to '+db);
 })
